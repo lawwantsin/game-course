914c86b56f5d1367219f0683c5219a9d2fb50c46
Lawrence Whiteside <law@cinemasetfree.com>
2019-11-30 14:28:19 -0600

    quick fix

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
 import "./index.css"
 
-const boot = () => {
-  const canvas = document.querySelector("canvas");
-  const context = canvas.getContext("2d");
-
-  const resize = () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+class Graphics {
+  constructor() {
+    this.canvas = document.querySelector("canvas");
+    this.context = this.canvas.getContext("2d");
+    window.addEventListener('resize', this.resize);
   }
+  resize() {
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+  }
+  validate() {
+    const args = Array.from(arguments);
+    args.map((a, i) => {
+      if (typeof a === 'undefined' || a == null) {
+        console.error(`Canvas Draw argument missing (${args.join(', ')})`);
+      }
+    });
+  }
+  rect(color, x, y, w, h) {
+    this.validate(color, x, y, w, h);
+    this.context.fillStyle = color;
+    this.context.fillRect(x, y, w, h);
+  }
+}
 
-  window.addEventListener('resize', resize);
-  resize();
-  context.fillStyle = 'green';
-  context.fillRect(0, 0, canvas.width, canvas.height);
+
+const boot = () => {
+  const G = new Graphics()
+  const w = G.canvas.width
+  const h = G.canvas.height
+  G.resize();
+  G.rect('black', 0, 0, w, h)
+  G.rect('red', w/2, h/2, 100, 100)
 }
 
 window.onload = boot
